0126d3b5da7b356a1a21f98d3d9ae1baf7e09818
coder7475 <robiulhossain7475@gmail.com>
2024-02-27 23:17:20 +0600

    feat: connect the counter slice with store using reducer

--- src/redux/features/counter/counterSlice.jsx
+++ src/redux/features/counter/counterSlice.jsx
@@ -8,6 +8,13 @@ export const counterSlice = createSlice({
     name: "counter",
     initialState,
     reducers: {
-        
+        increment: (state) => {
+            state.value += 1
+        },
+        decrement: (state) => {
+            state.value -= 1
+        }
     }
 })
+
+export default counterSlice.reducer;
