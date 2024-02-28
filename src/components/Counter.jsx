0a9123b225915125eb5669832e3854ef0570b55c
coder7475 <robiulhossain7475@gmail.com>
2024-02-28 08:55:44 +0600

    feat: in counter component get the counter value from store

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,15 @@
+import { useSelector } from "react-redux";
 
 const Counter = () => {
+    const count = useSelector((state) => state.counter.value);
+
     return (
-        <div>
-           <button>Increment</button>
+        <div className="flex gap-2 justify-center items-center">
+           <button className="border-2 p-1">Increment</button>
            <div>
-                <h1>0</h1>
+                <h1>{count}</h1>
            </div>
-           <button>Increment</button>
+           <button className="border-2 p-1">Decrement</button>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Counter from "../components/Counter";
 
 const Home = () => {
     return (
         <div>
-            <h1 className="text-5xl font-semibold text-purple-500">Home</h1>
+            <h1 className="text-5xl font-semibold text-purple-500 text-center my-4">Home</h1>
+            <Counter/>
         </div>
     );
 };
