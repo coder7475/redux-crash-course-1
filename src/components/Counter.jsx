11e2d1b1c4b5528750f88955ce77ecaf6a4ab986
coder7475 <robiulhossain7475@gmail.com>
2024-02-28 09:00:39 +0600

    feat: destructure the state from store slice

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,17 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
-    const count = useSelector((state) => state.counter.value);
+    // const count = useSelector((state) => state.counter.value);
+    const { value } = useSelector((state) => state.counter);
+
+    // output
+    const dispatch = useDispatch(); 
 
     return (
         <div className="flex gap-2 justify-center items-center">
            <button className="border-2 p-1">Increment</button>
            <div>
-                <h1>{count}</h1>
+                <h1>{value}</h1>
            </div>
            <button className="border-2 p-1">Decrement</button>
         </div>
