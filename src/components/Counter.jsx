51ed9d6bf41242d17c483da9d44029da7ff49f2f
coder7475 <robiulhossain7475@gmail.com>
2024-02-28 08:48:08 +0600

    feat: create counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,11 @@
 const Counter = () => {
     return (
         <div>
-            Counter            
+           <button>Increment</button>
+           <div>
+                <h1>0</h1>
+           </div>
+           <button>Increment</button>
         </div>
     );
 };
