a7ac3d3064585fee676ee49aa4d5d85c2fc6c26f
coder7475 <robiulhossain7475@gmail.com>
2024-02-29 22:51:19 +0600

    feat: create a custom logger - 2

--- src/redux/middlewares/logger.js
+++ src/redux/middlewares/logger.js
@@ -1,6 +1,12 @@
 const logger = (store) => (next) => (action) => {
     const state = store.getState();
-    console.log(state);
+    
+    console.log("current state => ", state);
+    console.log("Action dispatched => ", action);
+
+    next(action);
+
+    console.log("Updated state => ", store.getState());
 }
 
 export default logger;
